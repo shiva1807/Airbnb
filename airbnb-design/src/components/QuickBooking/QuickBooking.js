bac112f7b1484ce2a8c7d34e74d478bcf1f3eac8
Ashutosh Panda <iamashutoshpanda@gmail.com>
2019-07-13 17:06:48 +0530

    Quick Booking Tab (Without Date -Picker)

--- airbnb-design/src/components/QuickBooking/QuickBooking.js
+++ airbnb-design/src/components/QuickBooking/QuickBooking.js
@@ -8,7 +8,50 @@ class QuickBooking extends Component {
   }
 
   render() {
-    return "QuickBooking";
+    return (
+      <div className="box hero-body-box">
+        <div className="container has-text-centered">
+          <div className="columns">
+            <div className="column is-four-fifths">
+              <h1 class="subtitle is-3 top-subtitle">
+                Book unique places to stay and things to do.
+              </h1>
+            </div>
+          </div>
+
+          <div className="field">
+            <h6 class="subtitle is-6 label is-pulled-left heading">Where</h6>
+            <div className="control">
+              <input
+                className="input is-medium"
+                type="text"
+                placeholder="AnyWhere"
+              />
+            </div>
+          </div>
+
+          <div className="field">
+            <h6 class="subtitle is-6 label is-pulled-left heading">GUESTS</h6>
+            <div className="control">
+              <div className="select is-medium">
+                <select>
+                  <option>GUESTS</option>
+                  <option>GUESTS</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div className="field is-grouped is-pulled-right">
+            <div className="control">
+              <button className="button button-Submit is-danger is-medium">
+                Search
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
