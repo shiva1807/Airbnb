51fb8541bc8ddb1597e33a434ab008545d11b14d
Ashutosh Panda <iamashutoshpanda@gmail.com>
2019-07-14 23:26:28 +0530

    build update

---
+++ airbnb-design/build/precache-manifest.d53aabd860a9156dc571fe0fca24518f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09f0982f52043b6e9fee17b2a0009616",
+    "url": "/Airbnb/airbnb-design/build/index.html"
+  },
+  {
+    "revision": "710789c3a7b00d78d05a",
+    "url": "/Airbnb/airbnb-design/build/static/css/2.160cff76.chunk.css"
+  },
+  {
+    "revision": "d062a6cfb7ed120e53c5",
+    "url": "/Airbnb/airbnb-design/build/static/css/main.b0872f74.chunk.css"
+  },
+  {
+    "revision": "710789c3a7b00d78d05a",
+    "url": "/Airbnb/airbnb-design/build/static/js/2.e793536b.chunk.js"
+  },
+  {
+    "revision": "d062a6cfb7ed120e53c5",
+    "url": "/Airbnb/airbnb-design/build/static/js/main.825c9c39.chunk.js"
+  },
+  {
+    "revision": "31c12570ac93bffe5008",
+    "url": "/Airbnb/airbnb-design/build/static/js/runtime~main.51eff4f0.js"
+  }
+]);
