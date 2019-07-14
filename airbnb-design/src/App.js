40e0d90a06ccd1dd3517c2324195befdb469650f
Ashutosh Panda <iamashutoshpanda@gmail.com>
2019-07-14 13:10:46 +0530

    sass setup done grid layout for components

--- airbnb-design/src/App.js
+++ airbnb-design/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-// import Navbar from "./components/Navbar/Navbar";
-// import Jumbotron from "./components/Jumbotron/Jumbotron";
-// import QuickBooking from "./components/QuickBooking/QuickBooking";
+import Navbar from "./components/Navbar/Navbar";
+import Jumbotron from "./components/Jumbotron/Jumbotron";
+import QuickBooking from "./components/QuickBooking/QuickBooking";
 import Card1 from "./components/Cards1/Cards";
 import Card2 from "./components/Cards2/Card";
 import "bulma/css/bulma.css";
@@ -9,10 +9,23 @@ import "bulma/css/bulma.css";
 function App() {
   return (
     <div>
-      {/* <Navbar /> */}
-      {/* <Jumbotron /> */}
-      <Card1 />
-      {/* <Card2 /> */}
+      <div className="columns is-multiline">
+        <div className="column">
+          <Jumbotron />
+        </div>
+      </div>
+      <div className="columns is-multiline">
+        <div className="column">
+          <QuickBooking />
+        </div>
+        <div className="column" />
+        <div className="column">
+          <Card1 />
+        </div>
+        <div className="column">
+          <Card2 />
+        </div>
+      </div>
     </div>
   );
 }
